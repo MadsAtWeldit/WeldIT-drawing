4defe425534740acd1a0f73aa519dfd89e34f18c
MadsAtWeldit <mads@weldit.no>
2023-04-17 09:26:07 +0200

    Updates

---
+++ index.js
@@ -0,0 +1,167 @@
+"use strict";
+// const canvas = <HTMLCanvasElement>document.getElementById("drawing-board");
+const toolBar = document.getElementById("toolbar");
+//Get context of canvas
+// const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+// //Offset of canvas
+// enum Offset {
+//   X = canvas.offsetLeft,
+//   Y = canvas.offsetTop,
+// }
+// //Set canvas width and height
+// canvas.width = window.innerWidth - Offset.X;
+// canvas.height = window.innerHeight - Offset.Y;
+// let isDrawing: boolean;
+// let lineWidth = 5;
+// let startX: number;
+// let startY: number;
+// //Function runs whenever the mouse moves
+// const draw = (e: MouseEvent) => {
+//   if (!isDrawing) return;
+//   console.log("drawing");
+//   //Set linewidth and cap
+//   ctx.lineWidth = lineWidth;
+//   ctx.lineCap = "round";
+//   //Create line based on client mouse position
+//   ctx.lineTo(e.clientX - Offset.X, e.clientY);
+//   //Set stroke
+//   ctx.stroke();
+// };
+// //Listen for changes
+// toolBar.addEventListener("change", (e) => {
+//   //We know that target will be Input element so we type cast
+//   const target = e.target as HTMLInputElement;
+//   //IF Stroke
+//   if (target.id === "stroke") {
+//     //Set strokestyle
+//     ctx.strokeStyle = target.value;
+//   }
+//   //IF Linewidth
+//   if (target.id === "lineWidth") {
+//     //Set linewidth
+//     lineWidth = Number(target.value);
+//   }
+// });
+// toolBar.addEventListener("click", (e) => {
+//   const target = e.target as HTMLButtonElement;
+//   if (target.id === "clear") {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   }
+// });
+// //When mouse is held down
+// canvas.addEventListener("mousedown", (e) => {
+//   //Set drawing to true
+//   isDrawing = true;
+//   //Store starting point
+//   startX = e.clientX;
+//   startY = e.clientY;
+//   console.log("starting point set");
+// });
+// //Whenever we let go of mouse
+// canvas.addEventListener("mouseup", (e) => {
+//   //No longer painting
+//   isDrawing = false;
+//   //Save stroke
+//   ctx?.stroke();
+//   //Set or begin new path
+//   ctx?.beginPath();
+// });
+// //Listen for mousemove on canvas
+// canvas.addEventListener("mousemove", draw);
+class DrawingCanvas {
+    constructor(elementId, options) {
+        //Controller Change handler
+        this.changeHandler = (e) => {
+            const context = this.context;
+            const target = e.target;
+            if (target.id === "stroke") {
+                context.strokeStyle = target.value;
+            }
+            if (target.id === "lineWidth") {
+                context.lineWidth = Number(target.value);
+            }
+        };
+        //Controller Clear canvas
+        this.clearCanvas = (e) => {
+            const context = this.context;
+            const target = e.target;
+            if (target.id === "clear") {
+                context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            }
+        };
+        //Runs whenever mouse is clicked
+        this.setDrawpoint = (e) => {
+            const evtType = e.touches
+                ? e.touches[0]
+                : e;
+            this.isDrawing = true;
+            const mouseX = evtType.clientX - this.canvas.offsetLeft;
+            const mouseY = evtType.clientY - this.canvas.offsetTop;
+        };
+        //Runs whenever mouse is released
+        this.stopDrawing = () => {
+            this.isDrawing = false;
+            //Save stroke
+            this.context.stroke();
+            //New Path
+            this.context.beginPath();
+        };
+        //Runs whenever mouse moves
+        this.draw = (e) => {
+            if (!this.isDrawing)
+                return;
+            //Check if event has touch or mouse and assign accordingly
+            const evtType = e.touches
+                ? e.touches[0]
+                : e;
+            this.context.lineWidth = this.lineWidth;
+            this.context.lineCap = "round";
+            this.context.lineTo(evtType.clientX - this.canvas.offsetLeft, evtType.clientY - this.canvas.offsetTop);
+            //Save stroke
+            this.context.stroke();
+        };
+        //Get element access based on id passed
+        const canvas = document.getElementById(elementId);
+        const context = canvas.getContext("2d");
+        //IF a controller is passed
+        if (options === null || options === void 0 ? void 0 : options.controllerId) {
+            const controller = document.getElementById(options.controllerId);
+            this.controller = controller;
+        }
+        //Check if width and height has been set
+        (options === null || options === void 0 ? void 0 : options.width)
+            ? (canvas.width = options.width)
+            : (canvas.width = window.innerWidth - canvas.offsetLeft);
+        (options === null || options === void 0 ? void 0 : options.height)
+            ? (canvas.height = options.height)
+            : (canvas.height = window.innerHeight - canvas.offsetTop);
+        //Assign private props
+        this.canvas = canvas;
+        this.context = context;
+        //Default values
+        this.context.lineWidth = 5;
+        this.context.strokeStyle = "black";
+        //Add eventlisteners to canvas
+        this.listen();
+    }
+    //Listen for events on given canvas
+    listen() {
+        const canvas = this.canvas;
+        const controller = this.controller;
+        canvas.addEventListener("mousedown", this.setDrawpoint);
+        canvas.addEventListener("mouseup", this.stopDrawing);
+        canvas.addEventListener("mousemove", this.draw);
+        canvas.addEventListener("touchstart", this.setDrawpoint);
+        canvas.addEventListener("touchend", this.stopDrawing);
+        canvas.addEventListener("touchmove", this.draw);
+        controller.addEventListener("change", this.changeHandler);
+        controller.addEventListener("click", this.clearCanvas);
+    }
+    log() {
+        return console.log(this.canvas);
+    }
+}
+new DrawingCanvas("drawing-board", {
+    controllerId: "toolbar",
+});
+//# sourceMappingURL=index.js.map
